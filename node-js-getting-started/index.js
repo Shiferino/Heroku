60da9cc2b7dcfda1d6ed2ccd0af4cf4860f4d1bf
Shiferino <45627140+Shiferino@users.noreply.github.com>
2019-01-23 23:18:25 +0100

    buercher2

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const cool = require('cool-ascii-faces')
-const path = require('path')
-const PORT = process.env.PORT || 8080
+ var express = require('express'); 
+var app = express();
+const PORT = process.env.PORT || 8080;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req,res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var buecher = [
+{autor: "Max Schwerdtner",titel: "PHP-Kochbuch",    Beschreibung: "Das beste Buch zum kochen mit php überhaupt!" },
+{autor: "Marvin Spath",titel: "HTML-Kochbuch",    Beschreibung: "Das beste Buch zum kochen mit html überhaupt!"},
+{autor: "Winston Schilling", titel: "Java-Kochbuch",    Beschreibung: "Das beste Buch zum kochen mit java überhaupt!"}
+];
+
+app.set("viewengine", "ejs");
+app.get("/",function(req,res){
+res.render("buecher.ejs", {buecher}); });
+
+app.use(function(request, response) {  // Fehlerbehandlung 
+response.status(404).send("Seite nicht gefunden");
+}); 
+var server = app.listen(PORT, function () {
+   var host = server.address().address
+   var port = server.address().port
+      console.log("Example app listening at http://%s:%s", host, port)
+});
